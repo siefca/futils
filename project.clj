4bc40a477bf667872ec8522158af602f51201910
Paweł Wilk <siefca@gnu.org>
2015-10-28 17:29:37 +0100

    Documentation configuration updated

--- project.clj
+++ project.clj
@@ -1,16 +1,34 @@
 (defproject pl.randomseed/futils "0.1.0"
   :description "Function Utilities library"
   :url "https://randomseed.pl/en/software/futils"
+  
   :license {:name "LGPL License", :url "https://opensource.org/licenses/lgpl-3.0.html"}
   :scm     {:name "git", :url "https://github.com/siefca/futils"}
+  
   :dependencies [[org.clojure/clojure "1.7.0"]]
+  
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
                    :plugins [[lein-midje "3.1.3"]
                              [lein-midje-doc "0.0.23"]]}}
-  :documentation {:files {"docs/index"
+  
+  :documentation {:site   "futils"
+                  :description "Function Utilities Library"
+                  :owners [{:name    "Paweł Wilk"
+                            :email   "pw@gnu.org"
+                            :website "https://randomseed.pl/"}]
+                  :paths ["src-doc"]
+                  :files {"docs/index"
                           {:input     "src-doc/futils/overview.clj"
                            :title     "futils"
-                           :sub-title "Function Utilities Library"
+                           :subtitle  "Function Utilities Library"
                            :author    "Paweł Wilk"
-                           :email     "pw@gnu.org"}}}
+                           :email     "pw@gnu.org"}}
+                  :html {:home "index",
+                         :navigation ["home"
+                                      {:link "https://github.com/siefca/futils",
+                                       :text "source"}]}
+                  :link {:auto-tag    true
+                         :auto-number true}}
+  
   :global-vars {*warn-on-reflection* true})
+
