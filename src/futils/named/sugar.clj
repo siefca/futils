5395da59671deb77717f75c1f0d38c4a98631aef
Paweł Wilk <siefca@gnu.org>
2015-12-04 19:18:56 +0100

    Begin work on nameized versions of built-in core functions

---
+++ src/futils/named/sugar.clj
@@ -0,0 +1,33 @@
+(ns
+
+    ^{:doc    "futils library, nameized built-in functions."
+      :author "Paweł Wilk"}
+
+    futils.named.sugar
+
+  (:refer-clojure :only [fn defn if-let when cond and or])
+  (:require [clojure.core :as c]
+            [futils.named :refer :all]
+            [futils.utils :refer :all]))
+
+(defn reduce
+  {:added "0.8"}
+  [& {:keys [:f :coll :val] :as args}]
+  (if-let [coll (c/find :coll args)]
+    (c/reduce f val coll)
+    (c/reduce f coll)))
+
+(defn map
+  {:added "0.8"}
+  [& {:keys [:f :coll :colls] :as args}]
+  (if-let [colls (c/find :colls args)]
+    (c/apply c/map f coll colls)
+    (c/map f coll)))
+
+(def apply
+  ^{:added "0.8"}
+  mapply)
+
+(defn list
+  [& {:keys [:items]}]
+  (c/apply c/list items))
